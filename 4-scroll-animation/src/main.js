95bdfd440122170a3e055b15397a47b10a049a16
Passionhruit <korea_stan@naver.com>
2024-06-21 17:58:14 +0900

    feat: 일렁이는 파도효과 추가

---
+++ 4-scroll-animation/src/main.js
@@ -0,0 +1,107 @@
+import * as THREE from "three";
+
+window.addEventListener("load", function () {
+  init();
+});
+
+function init() {
+  const canvas = document.querySelector("#canvas");
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    canvas,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const scene = new THREE.Scene();
+
+  /** fog 속성 */
+
+  scene.fog = new THREE.Fog(0xf0f0f0, 0.1, 500);
+
+  const camera = new THREE.PerspectiveCamera(
+    75, // field of view
+    window.innerWidth / window.innerHeight, // 종횡비
+    1, // near
+    500 // far
+  );
+
+  camera.position.set(0, 25, 150);
+
+  const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 150, 150);
+  const waveMaterial = new THREE.MeshStandardMaterial({
+    // wireframe: true,
+    color: "#00ffff",
+  });
+
+  const wave = new THREE.Mesh(waveGeometry, waveMaterial);
+
+  wave.rotation.x = -Math.PI / 2;
+
+  // 파도모양만들기
+
+  const waveHeight = 2.5;
+
+  const initialZPositions = [];
+
+  for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
+    const z =
+      waveGeometry.attributes.position.getZ(i) +
+      (Math.random() - 0.5) * waveHeight;
+
+    waveGeometry.attributes.position.setZ(i, z);
+    initialZPositions.push(z);
+  }
+
+  wave.update = function () {
+    const elapsedTime = clock.getElapsedTime();
+
+    for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
+      const z =
+        initialZPositions[i] + Math.sin(elapsedTime * 3 + i ** 2) * waveHeight;
+
+      waveGeometry.attributes.position.setZ(i, z);
+    }
+
+    waveGeometry.attributes.position.needsUpdate = true;
+  };
+
+  scene.add(wave);
+
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(15, 15, 15);
+  scene.add(pointLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+  directionalLight.position.set(-15, 15, 15);
+  scene.add(directionalLight);
+
+  const clock = new THREE.Clock();
+
+  render();
+
+  // render animation 효과
+
+  function render() {
+    wave.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  // 반응형 renderer resizing
+
+  function handleResize() {
+    // 큐브 크기비율도 맞춰서 자연스럽게 리사이징
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render();
+  }
+
+  window.addEventListener("resize", handleResize);
+}
